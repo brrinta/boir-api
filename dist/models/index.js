07b4af3a9aa20b1a8910ccbfa85934b46552343e
S Brrinta <brrinta@gmail.com>
2024-04-15 23:16:26 -0400

    init

---
+++ dist/models/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./attachments"), exports);
+__exportStar(require("./attachments-error"), exports);
+__exportStar(require("./attachments-error-error"), exports);
+__exportStar(require("./inline-response200"), exports);
+__exportStar(require("./inline-response2001"), exports);
+__exportStar(require("./inline-response2002"), exports);
+__exportStar(require("./inline-response400"), exports);
+__exportStar(require("./process-id"), exports);
+__exportStar(require("./submission-accepted"), exports);
+__exportStar(require("./submission-incorrect-process-id"), exports);
+__exportStar(require("./submission-rejected"), exports);
+__exportStar(require("./submission-status"), exports);
+__exportStar(require("./submission-validation-failed"), exports);
+__exportStar(require("./submissionaccepted-errors"), exports);
+__exportStar(require("./submissionincorrectprocess-id-error"), exports);
+__exportStar(require("./submissionrejected-errors"), exports);
+__exportStar(require("./submissionvalidationfailed-validation-errors"), exports);
+__exportStar(require("./transcript"), exports);
+__exportStar(require("./transcript-status"), exports);
+__exportStar(require("./upload-error"), exports);
+__exportStar(require("./upload-success"), exports);
+__exportStar(require("./uploaderror-error"), exports);
